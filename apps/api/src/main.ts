e9c96906010053fb3fc824c02c21aa5d59dd2436
peterwiebe <pjwiebe@gmail.com>
2021-04-07 09:16:21 -0700

    Add basic security defaults

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,12 +5,13 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import * as helmet from 'helmet';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
