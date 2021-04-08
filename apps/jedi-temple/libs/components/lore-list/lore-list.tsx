8590f10edeec3c3371d4667383cc7732b82f7d39
peterwiebe <pjwiebe@gmail.com>
2021-04-08 14:42:25 -0700

    Enable carousel scrolling

--- apps/jedi-temple/libs/components/lore-list/lore-list.tsx
+++ apps/jedi-temple/libs/components/lore-list/lore-list.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { getId } from '@coruscant/utils/get-id';
 
-import { Typography } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { Button, Typography } from 'antd';
 
 import styles from './lore-list.module.scss';
 
@@ -10,27 +11,66 @@ const { Title } = Typography;
 export interface LoreListProps {
   list?: string[];
   render: any;
+  shift: number;
   title: string;
 }
 
-export function PlanetList({ list, render: Component, title }: LoreListProps) {
+export function PlanetList({
+  list,
+  render: Component,
+  shift = 3,
+  title,
+}: LoreListProps) {
   const [index, setIndex] = React.useState(0);
 
   const handleForwardClick = () => {
-    setIndex(index + 1);
+    setIndex(index + shift);
   };
   const handleBackwardClick = () => {
-    setIndex(index - 1);
+    if (index > 0) {
+      setIndex(index - shift);
+    }
   };
 
   return (
     <div>
       <Title level={3}>{title}</Title>
 
-      <div className={styles.loreList}>
-        <Component id={getId(list?.[index]) || ''} />
-        <Component id={getId(list?.[index + 1]) || ''} />
-        <Component id={getId(list?.[index + 2]) || ''} />
+      <div style={{ position: 'relative' }}>
+        <div
+          className={styles.button}
+          style={{
+            left: '-2.5rem',
+          }}
+        >
+          <Button
+            disabled={index === 0}
+            shape="circle"
+            icon={<LeftOutlined />}
+            onClick={handleBackwardClick}
+          />
+        </div>
+        <div className={styles.loreList}>
+          <Component id={getId(list?.[index]) || ''} />
+          {index + 1 < list.length ? (
+            <Component id={getId(list?.[index + 1]) || ''} />
+          ) : (
+            <div />
+          )}
+          {index + 2 < list.length ? (
+            <Component id={getId(list?.[index + 2]) || ''} />
+          ) : (
+            <div />
+          )}
+        </div>
+        <div className={styles.button} style={{ right: '-2.5rem', top: 0 }}>
+          <Button
+            disabled={(index / shift + 1) * 3 >= list.length}
+            shape="circle"
+            icon={<RightOutlined />}
+            onClick={handleForwardClick}
+          />
+        </div>
       </div>
     </div>
   );
